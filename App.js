7e26b9f3a37ec7c83603d2ea418fd8b75c210320
smironescu <sergio.mironescu@marfeel.com>
2019-02-07 18:30:58 +0100

    added facebook login

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import store from './location-app/store';
+import PreLoader from './location-app/components/PreLoader';
+import * as firebase from 'firebase';
+import { firebaseConfig } from './location-app/constants/firebase';
+import MainGuestScreen from './location-app/screens/guests/MainGuestScreen';
+import MainUserScreen from './location-app/screens/users/MainUserScreen';
+import Toast, { DURATION } from 'react-native-easy-toast';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+firebase.initializeApp(firebaseConfig);
+
+export class App extends Component {
+	constructor() {
+		super();
+
+		this.refToast = React.createRef();
+
+		this.state = {
+			isLogged: false,
+			loaded: false
+		};
+	}
+
+	async componentDidMount() {
+		// For Testing NOT logged
+		firebase.auth().signOut();
+
+		await firebase.auth().onAuthStateChanged(user => {
+			if (user) {
+				this.setState({
+					isLogged: true,
+					loaded: true
+				});
+
+				this.refToast.current.show('Successfully Authenticated!', DURATION.LENGTH_LONG);
+			} else {
+				this.setState({
+					isLogged: false,
+					loaded: true
+				});
+			}
+		});
+	}
+
+
+	render() {
+		const { isLogged, loaded } = this.state;
+
+		if (!loaded) {
+			return (<PreLoader />);
+		} else if (isLogged) {
+			return (
+				<Provider store={store}>
+					<Toast ref={this.refToast} />
+					<MainUserScreen />
+				</Provider>
+			);
+		}
+
+		return (
+			<Provider store={store}>
+				<MainGuestScreen />
+			</Provider>
+		);
+	}
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
